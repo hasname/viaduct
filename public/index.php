52f1696229a84575d2a59e77c8d07d8fb119ce7b
Gea-Suan Lin <gslin@gslin.org>
2025-06-27 16:25:58 +0800

    style: fix make lint errors.

--- public/index.php
+++ public/index.php
@@ -1,2 +1,15 @@
 <?php
+
+/**
+ * Main entry point that redirects to the GitHub repository.
+ *
+ * PHP version 8.0
+ *
+ * @category Application
+ * @package  Viaduct
+ * @author   Gea-Suan Lin <gslin@gslin.com>
+ * @license  MIT License
+ * @link     https://github.com/hasname/viaduct
+ */
+
 header('Location: https://github.com/hasname/viaduct');
