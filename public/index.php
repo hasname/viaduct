29ee03a8259b8fbbe8d0757c1e7e4f6728d9d2ec
Gea-Suan Lin <gslin@gslin.org>
2025-07-05 04:41:09 +0800

    docs: add comment about 404 handler.

--- public/index.php
+++ public/index.php
@@ -5,5 +5,8 @@ require __DIR__ . '/../vendor/autoload.php';
 use Slim\Factory\AppFactory;
 
 $app = AppFactory::create();
+
+// Add 404 handler.
 $app->addErrorMiddleware(false, false, false);
+
 $app->run();
